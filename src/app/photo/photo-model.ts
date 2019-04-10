77064f54c60582b49707f59622403722a01ec764
SDev03 <dev2523@hotmail.com>
2019-04-10 10:11:56 +0800

    User Model page

---
+++ src/app/photo/photo-model.ts
@@ -0,0 +1,24 @@
+export interface User {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+}
+
+export interface Address {
+  street: string ;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+export interface Geo {
+  lat: string;
+  lng: string;
+}
+
+
+
+
